121d763876b8ed57f55aace35888df7a427813c9
Igor Galvão Dias <105743982+IgorGDias1@users.noreply.github.com>
2024-04-12 21:59:22 -0300

    Inicio do Projeto

---
+++ admin/actions/classes/Categoria.class.php
@@ -0,0 +1,50 @@
+<?php
+require_once('Banco.class.php');
+
+class Categoria{
+    public $id;
+    public $nome;
+
+    public function Listar(){
+        $sql = "SELECT * FROM categorias";
+        $banco = Banco::conectar();
+        $comando = $banco->prepare($sql);
+        $comando->execute();
+
+        $arr_resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
+        Banco::desconectar();
+
+        return $arr_resultado;
+    }
+
+    public function Cadastrar(){
+        $sql = "INSERT INTO categorias(nome) 
+        VALUES (?)";
+        $banco = Banco::conectar();
+        $comando = $banco->prepare($sql);
+        $comando->execute([$this->nome]);
+        Banco::desconectar();
+        return $comando->rowCount();
+    }
+
+    public function Modificar(){
+        $sql = "UPDATE categorias SET nome=? WHERE id=?";
+        $banco = Banco::conectar();
+        $comando = $banco->prepare($sql);
+        $comando->execute([$this->nome, $this->id]);
+        Banco::desconectar();
+        return $comando->rowCount();
+    }
+
+    public function Remover(){
+        $sql = "DELETE FROM categorias WHERE id = ?";
+        $banco = Banco::conectar();
+        $comando = $banco->prepare($sql);
+        $comando->execute([$this->id]);
+        Banco::desconectar();
+        return $comando->rowCount();
+    }
+}
+
+
+?>
